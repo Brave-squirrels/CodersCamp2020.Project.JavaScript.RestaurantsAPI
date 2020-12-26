96dabd3efb550c350adca54e16ad74c0fae39ccf
Veryape <veryyapeee@gmail.com>
2020-12-26 19:17:30 +0100

    Done basic landing-page setup

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@
 document.querySelector('#favTrigger').addEventListener('click',()=>{
     const favList = document.querySelector('#favList');
     favList.style.width = '100%';
+    favList.style.right = '0';
 })
 
 //Hide Fav menu
 document.querySelector('#closeFav').addEventListener('click', ()=>{
     const favList = document.querySelector('#favList');
     favList.style.width = '0';
+    favList.style.right = '-2em';
 })
