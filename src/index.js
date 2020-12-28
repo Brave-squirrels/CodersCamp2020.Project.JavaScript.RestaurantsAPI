7318a4b8e7732d44f90eb2e22d5ca524396c5dce
Veryape <veryyapeee@gmail.com>
2020-12-28 10:08:33 +0100

    optimization

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 //Display Fav menu
 document.querySelector('#favIcon').addEventListener('click',()=>{
-    const favList = document.querySelector('#favList');
+    const favList = document.querySelector('aside');
     favList.style.width = '100%';
     favList.style.right = '0';
 })
 
 //Hide Fav menu
 document.querySelector('#closeFav').addEventListener('click', ()=>{
-    const favList = document.querySelector('#favList');
+    const favList = document.querySelector('aside');
     favList.style.width = '0';
     favList.style.right = '-4em';
 })
