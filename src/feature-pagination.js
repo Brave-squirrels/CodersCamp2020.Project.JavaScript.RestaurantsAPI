9a501d0306455591661cd48faf2beae409a89583
Veryape <veryyapeee@gmail.com>
2020-12-28 21:17:13 +0100

    Working pagination feature

--- src/feature-pagination.js
+++ src/feature-pagination.js
@@ -1,6 +1,5 @@
 //Arary with the elements
 let arr =[
-    
         `<span>hello1</span>`,
         `<span>hello2</span>`,
         `<span>hello3</span>`,
@@ -9,60 +8,57 @@ let arr =[
         `<span>hello6</span>`,
         `<span>hello7</span>`,
         `<span>hello8</span>`,
-    
+        `<span>hello1</span>`,
+        `<span>hello2</span>`,
+        `<span>hello3</span>`,
+        `<span>hello4</span>`,
+        `<span>hello5</span>`,
+        `<span>hello6</span>`,
+        `<span>hello7</span>`,
+        `<span>hello8</span>`,
 ]
 
-//State which will be updated
+//State setup with pagination option
 const state = {
+    //Elements on page
     'pageSize': 3,
+    //On which page it starts
     'pageNumber': 1
 }
 
-//generate slice of the array that we wanna display
-const paginate = (obj, pageSize, pageNumber)=>{   
-    
+//Generate slice of the array that we wanna display (depends on setup in state object)
+const paginate = (obj, pageSize, pageNumber) => {   
     let current = (pageNumber-1)*pageSize;
     let endData = current + pageSize;
-    
     return obj.slice(current, endData);
 }
 
-//generate buttons
-const generatePage = ()=>{
+//Generate buttons depending on which page we are
+const generatePage = () => {
     const buttons = document.querySelector('#pages');
-        if(state.pageNumber === 1){
-        buttons.innerHTML = ` <button id='btn2'>Page ${state.pageNumber+1}</button>`;
-        }
-        
-        else if(state.pageNumber>1 && state.pageNumber < Math.ceil(arr.length/state.pageSize)){      
-        buttons.innerHTML = `
-            <button id='btn1'>Page ${state.pageNumber-1}</button> <br>
-            <button id='btn2'>Page ${state.pageNumber+1}</button>
-        `;
-        }else{
-            buttons.innerHTML = `<button id='btn1'>Page ${state.pageNumber-1}</button>`;
-        }
+    const maxPage = Math.ceil(arr.length/state.pageSize);
+    buttons.innerHTML = (state.pageNumber === 1 ) ? `<button id='btn2'>Page ${state.pageNumber+1}</button>` :
+                        (state.pageNumber === maxPage ) ? `<button id='btn1'>Page ${state.pageNumber-1}</button>` :
+                        `<button id='btn1'>Page ${state.pageNumber-1}</button> <br> <button id='btn2'>Page ${state.pageNumber+1}</button>`;
 }
 
-//appending elements the the DOM
+//Update DOM by appending current data
 const appendData = (result)=>{
     generatePage();
-    let divData = document.querySelector('#data');
+    const divData = document.querySelector('#data');
     divData.innerHTML = result;
 }
 
-//default data when reloading the site
-appendData(paginate(arr,state.pageSize, state.pageNumber));
+//Default data when reloading the site - envoke this in filter/search function as a setup
+appendData(paginate(arr, state.pageSize, state.pageNumber));
 
-
-//switch pages
+//Switch pages events
 document.addEventListener('click', (e)=>{
     if(e.target && e.target.id== 'btn2'){
         state.pageNumber += 1;
-        appendData(paginate(arr,state.pageSize, state.pageNumber));
+        appendData(paginate(arr, state.pageSize, state.pageNumber));
     }else if(e.target && e.target.id == 'btn1'){
-        state.pageNumber -=1;
-        appendData(paginate(arr,state.pageSize, state.pageNumber));
+        state.pageNumber -= 1;
+        appendData(paginate(arr, state.pageSize, state.pageNumber));
     }
 })
-
