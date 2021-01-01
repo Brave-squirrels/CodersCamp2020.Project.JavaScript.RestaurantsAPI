014d57fce7687515481aa5a84fabfc0d9dc5bf3d
Cyprian Zamiara <veryyapeee@gmail.com>
2021-01-01 19:55:04 +0100

    Finish pagination

--- src/feature-pagination.js
+++ src/feature-pagination.js
@@ -1,24 +1,3 @@
-//Arary with the elements
-let arr =[
-        `<span>hello1</span>`,
-        `<span>hello2</span>`,
-        `<span>hello3</span>`,
-        `<span>hello4</span>`,
-        `<span>hello5</span>`,
-        `<span>hello6</span>`,
-        `<span>hello7</span>`,
-        `<span>hello8</span>`,
-        `<span>hello1</span>`,
-        `<span>hello2</span>`,
-        `<span>hello3</span>`,
-        `<span>hello4</span>`,
-        `<span>hello5</span>`,
-        `<span>hello6</span>`,
-        `<span>hello7</span>`,
-        `<span>hello8</span>`,
-]
-
-//State setup with pagination option
 const state = {
     //Elements on page
     'pageSize': 3,
@@ -34,31 +13,36 @@ const paginate = (obj, pageSize, pageNumber) => {
 }
 
 //Generate buttons depending on which page we are
-const generatePage = () => {
-    const buttons = document.querySelector('#pages');
+const generatePage = (arr,btnContainer) => {
     const maxPage = Math.ceil(arr.length/state.pageSize);
-    buttons.innerHTML = (state.pageNumber === 1 ) ? `<button id='btn2'>Page ${state.pageNumber+1}</button>` :
+    btnContainer.innerHTML = (state.pageNumber === 1 ) ? `<button id='btn2'>Page ${state.pageNumber+1}</button>` :
                         (state.pageNumber === maxPage ) ? `<button id='btn1'>Page ${state.pageNumber-1}</button>` :
                         `<button id='btn1'>Page ${state.pageNumber-1}</button> <br> <button id='btn2'>Page ${state.pageNumber+1}</button>`;
 }
 
 //Update DOM by appending current data
-const appendData = (result)=>{
-    generatePage();
-    const divData = document.querySelector('#data');
-    divData.innerHTML = result;
+const appendData = (result, arr, btnContainer, dataContainer)=>{
+    generatePage(arr, btnContainer);
+    dataContainer.innerHTML = result;
 }
 
 //Default data when reloading the site - envoke this in filter/search function as a setup
-appendData(paginate(arr, state.pageSize, state.pageNumber));
+const append = (arr, btnContainer, dataContainer)=>{
+appendData(paginate(arr, state.pageSize, state.pageNumber), arr, btnContainer, dataContainer);
+}
 
 //Switch pages events
-document.addEventListener('click', (e)=>{
+
+
+const generateBtn = (e)=>{
     if(e.target && e.target.id== 'btn2'){
         state.pageNumber += 1;
-        appendData(paginate(arr, state.pageSize, state.pageNumber));
     }else if(e.target && e.target.id == 'btn1'){
         state.pageNumber -= 1;
-        appendData(paginate(arr, state.pageSize, state.pageNumber));
     }
-})
+}
+
+module.exports = {
+    generateBtn: generateBtn,
+    append: append
+}
