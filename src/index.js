67f1bd6139abd01f2ae57547530ebd2bbeea726a
AgataK92 <agata.kurowska92@gmail.com>
2021-01-01 23:44:22 +0100

    Implemented basics of the favourites mechanism; basic favourites page structure implemented.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const displayFav = require('../src/nav-bar');
+const favourites = require('../src/favourites-feature');
 
 //Handle the favList
 document.addEventListener('click', displayFav);
 
+window.addEventListener('load', (event) => {
+    console.log("Onload check log.");
+    //this returns HTML collection, therefore normal foreach cannot be used.
+    var checkboxes = document.getElementsByClassName('starFavInput');
+    Array.from(checkboxes).forEach((item) => {
+        item.checked = favourites.isFavourite(item.id);
+    });
+  });
