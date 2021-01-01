153e04f353bd0c9042c1967a5f7adb0472694f14
AgataK92 <agata.kurowska92@gmail.com>
2021-01-01 23:44:22 +0100

    Implemented basics of the favourites mechanism; basic favourites page structure implemented.

---
+++ src/favourites-feature.js
@@ -0,0 +1,44 @@
+var favouritesKey = 'FAVOURITE_RESTAURANTS'
+
+const manageFavouriteRestaurant = (restaurantId) => {
+    /* extract favourites from local storage */
+    let favourites = localStorage.getItem(favouritesKey);
+    /* If local storage empty -> add restaurant to favourites */
+    if (!favourites) {
+        localStorage.setItem(favouritesKey, restaurantId);
+        return
+    }
+        //  Else -> retrieve and split by comma
+    let favouritesArray = favourites.split(',');
+//Check if array contains id
+    // if (favouritesArray.indexOf(restaurantId) >= 0) {
+// if true -> rmove from array
+    let index = favouritesArray.indexOf(restaurantId);
+    if (index > -1) {
+        favouritesArray.splice(index, 1);
+    }
+// else -> add to array;
+    else {
+        favouritesArray.push(restaurantId);
+    }
+// then join array by comma and save (setItem)
+    favourites = favouritesArray.join(',');
+    localStorage.setItem(favouritesKey, favourites);
+}
+
+const isFavourite = (restaurantId) => {
+    /* extract favourites from local storage */
+    let favourites = localStorage.getItem(favouritesKey);
+    // If local storage empty -> return false
+    if (!favourites) {
+       return false;
+    }
+    // retrieve and split by comma
+    let favouritesArray = favourites.split(',');
+    //Check if array contains id - return boolean
+    let index = favouritesArray.indexOf(restaurantId);
+    if (index > -1) {
+        return true;
+    }
+    return false;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //Display Fav menu
 document.querySelector('#favIcon').addEventListener('click',()=>{
     const favList = document.querySelector('aside');
-    favList.style.width = '100%';
+    favList.style.width = '40%';
     favList.style.right = '0';
 })
 
@@ -11,3 +11,11 @@ document.querySelector('#closeFav').addEventListener('click', ()=>{
     favList.style.width = '0';
     favList.style.right = '-4em';
 })
+
+window.addEventListener('load', (event) => {
+    //this returns HTML collection, therefore normal foreach cannot be used.
+    var checkboxes = document.getElementsByClassName('starFavInput');
+    Array.from(checkboxes).forEach((item) => {
+        item.checked = isFavourite(item.id);
+    });
+  });
