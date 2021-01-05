9fa39677182a2c1e66f3e686e28d58a36c9f7f86
Cyprian Zamiara <veryyapeee@gmail.com>
2021-01-05 19:59:18 +0100

    Demo search view done

--- src/search-view.js
+++ src/search-view.js
@@ -36,118 +36,115 @@ function display(e){
                 console.log(`not ${inputValue}`);
             }else{
                 //Creating templates with data and pushing into array
-            container.style.display = 'grid';
-            final.forEach((n)=>{
-                //data-name - get this on click and base on that display restaurant
-                navData.push(`<div id='resDiv' class='resDiv' data-name="${n.id}">
-                <span class='resTitle'>
-                    ${n.name}
-                </span>
-                <span class='resCs'>
-                    ${n.rating}
-                </span>
-                <span class='resAdr'>
-                    ${n.address}
-                </span>
-                <img src="//cdn.clipartsfree.net/vector/small/50542-right-grey-arrow-icon.png" alt="" class='resImg'>
-                </div>`);
-            });
+                container.style.display = 'grid';
+                final.forEach((n)=>{
+                    //data-name - get this on click and base on that display restaurant
+                    navData.push(`<div id='resDiv' class='resDiv' data-name="${n.id}">
+                    <span class='resTitle'>
+                        ${n.name}
+                    </span>
+                    <span class='resCs'>
+                        ${n.rating}
+                    </span>
+                    <span class='resAdr'>
+                        ${n.address}
+                    </span>
+                    <img src="//cdn.clipartsfree.net/vector/small/50542-right-grey-arrow-icon.png" alt="" class='resImg'>
+                    </div>`);
+                });
 
-            //Save data to localStorage for future
-            localStorage.setItem("data", JSON.stringify(final));
+                //Save data to localStorage for future
+                localStorage.setItem("data", JSON.stringify(final));
 
-            //Default append data on the first site
-            append(navData, buttons, divData);
+                //Default append data on the first site
+                append(navData, buttons, divData);
 
-            //Get the array of all cuisines in the city
-            const cuisinesAll = [];
-            final.forEach((n)=>{
-                const cuisinesSplit = n.cuisines.split(',');
-                cuisinesSplit.forEach((n)=>{
-                    if(!cuisinesAll.includes(n)){
-                        cuisinesAll.push(n);
-                    }
-                })
-            })
-
-            //Display filters
-            cuisinesAll.forEach((n)=>{
-                filter.innerHTML += `
-                    <label for="${n}">${n}</label>
-                    <input type="checkbox" id="${n}" name="cuisineFilter" value="${n}" class="chkId">
-                `;
-            })
-
-            //Saving the array of data
-            let savedNavData = navData;
-            const filterRestaurants = (e)=>{
-                //Checking target of the event
-                if(e.target.className === 'chkId'){
-
-                    const filterArray = [];
-                    //Getting all the checkboxes
-                    const checkedValues = document.querySelectorAll('.chkId');
-
-                    //Pushing all checked value into the array
-                    [...checkedValues].forEach((n)=>{
-                        if(n.checked){
-                            filterArray.push(n.value);
+                //Get the array of all cuisines in the city
+                const cuisinesAll = [];
+                final.forEach((n)=>{
+                    const cuisinesSplit = n.cuisines.split(',');
+                    cuisinesSplit.forEach((n)=>{
+                        if(!cuisinesAll.includes(n)){
+                            cuisinesAll.push(n);
                         }
                     })
+                })
 
-                    let tmpNavData = [];
+                //Display filters
+                cuisinesAll.forEach((n)=>{
+                    filter.innerHTML += `
+                        <label for="${n}" id="filterLabel">${n}</label>
+                        <input type="checkbox" id="${n}" name="cuisineFilter" value="${n}" class="chkId">
+                    `;
+                })
 
-                    //Getting restaurants with matching cuisines
-                        final.forEach((n)=>{
-                            const splitArr = n.cuisines.split(',');
-                            const rez = filterArray.some(r => splitArr.includes(r));
-                            if(rez){
-                                tmpNavData.push(`<div id='resDiv' class='resDiv data-name="${n.id}"'>
-                                <span class='resTitle'>
-                                    ${n.name}
-                                </span>
-                                <span class='resCs'>
-                                    ${n.rating}
-                                </span>
-                                <span class='resAdr'>
-                                    ${n.address}
-                                </span>
-                                <img src="//cdn.clipartsfree.net/vector/small/50542-right-grey-arrow-icon.png" alt="" class='resImg'>
-                                </div>`);
+                //Saving the array of data
+                let savedNavData = navData;
+                const filterRestaurants = (e)=>{
+                    //Checking target of the event
+                    if(e.target.className === 'chkId'){
+
+                        const filterArray = [];
+                        //Getting all the checkboxes
+                        const checkedValues = document.querySelectorAll('.chkId');
+
+                        //Pushing all checked value into the array
+                        [...checkedValues].forEach((n)=>{
+                            if(n.checked){
+                                filterArray.push(n.value);
                             }
-
                         })
-                    
-                    //Resets data to default when unchecked
-                    if(tmpNavData.length !== 0){
-                        navData = tmpNavData;
-                    }else{
-                        navData=savedNavData;
+
+                        let tmpNavData = [];
+
+                        //Getting restaurants with matching cuisines
+                            final.forEach((n)=>{
+                                const splitArr = n.cuisines.split(',');
+                                const rez = filterArray.some(r => splitArr.includes(r));
+                                if(rez){
+                                    tmpNavData.push(`<div id='resDiv' class='resDiv data-name="${n.id}"'>
+                                    <span class='resTitle'>
+                                        ${n.name}
+                                    </span>
+                                    <span class='resCs'>
+                                        ${n.rating}
+                                    </span>
+                                    <span class='resAdr'>
+                                        ${n.address}
+                                    </span>
+                                    <img src="//cdn.clipartsfree.net/vector/small/50542-right-grey-arrow-icon.png" alt="" class='resImg'>
+                                    </div>`);
+                                }
+
+                            })
+                        
+                        //Resets data to default when unchecked
+                        if(tmpNavData.length !== 0){
+                            navData = tmpNavData;
+                        }else{
+                            navData=savedNavData;
+                        }
+                        //Displaying filtered data and generate new buttons
+                        generateBtn(e);
+                        append(navData, buttons, divData);
+
                     }
-                    //Displaying filtered data and generate new buttons
+                }
+
+                //Run filter
+                filter.addEventListener('click', filterRestaurants);
+
+                //To append pass array with data, element that will contain the buttons, element that will contain data
+                //Add event to generate buttons
+                document.addEventListener('click', (e)=>{
                     generateBtn(e);
                     append(navData, buttons, divData);
-
-                }
-            }
-
-            //Run filter
-            filter.addEventListener('click', filterRestaurants);
-
-            //To append pass array with data, element that will contain the buttons, element that will contain data
-            //Add event to generate buttons
-            document.addEventListener('click', (e)=>{
-                generateBtn(e);
-                append(navData, buttons, divData);
-            });
-            
-            //Scroll to the nav after submit
-            container.scrollIntoView();
+                });
+                
+                //Scroll to the nav after submit
+                container.scrollIntoView();
             }
         })
-    
-
-    
 }
 
 //Exporting main function
