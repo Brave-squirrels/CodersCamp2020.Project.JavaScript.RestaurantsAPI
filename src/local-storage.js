019d54e4e3def82805062c8fab48359851b04f00
MichalS <michal.smalira@gmail.com>
2021-01-06 10:09:40 +0100

    saving variables to cookies fo 24hrs

--- src/local-storage.js
+++ src/local-storage.js
@@ -1,5 +1,12 @@
-const saveToLocalStorage = async(restaurants, cityName) => {
-    await localStorage.setItem(`restaurants in ${cityName}`, JSON.stringify(restaurants));
+const saveToLocalStorage = (restaurants, cityName) => {
+    /*
+        @ saves restuarants json in current city to cookies for 24hrs
+    */
+
+    let nextDay = new Date();
+    nextDay.setDate(new Date().getDate() + 1);
+
+    document.cookie = `restaurants in ${cityName} = ${JSON.stringify(restaurants)}; expires = ${nextDay}`
 };
 
 module.exports = saveToLocalStorage;
