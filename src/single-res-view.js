4301a9a4afb8be8967d9cd76a7c92c00efab5ae2
Cyprian Zamiara <veryyapeee@gmail.com>
2021-01-13 14:32:46 +0100

    Single res function demo

--- src/single-res-view.js
+++ src/single-res-view.js
@@ -7,8 +7,8 @@ const obj = {
 
 }
 
-const createTemplate = (resObj) => {
-
+//template for basic info
+const createTemplateFirst = (resObj) => {
     return `
     <div class="restaurantIntro">
 
@@ -40,11 +40,42 @@ const createTemplate = (resObj) => {
 
     <button id='displayReviews' class='displayReviews'>Reviews</button>
     `
-    
+}
 
+//template for reviews - add loop for display reviews
+const createTemplateSecond = ()=>{
+    return `
+    <div id="reviews">
+    <div class='reviews'>
+        <!--Append review + te z localStorage-->
+        <span class="reviewsTitle">
+            Reviews
+        </span>
+        <!--Pull of of API-->
+        <div class="singleReview">
+            <span class="dish">
+                "The best restaurant i've ever been!"
+            </span>
+            <span class="cost">
+                5/5
+            </span>
+        </div>
+        <div class="singleReview">
+            <span class="dish">
+                "It's fine."
+            </span>
+            <span class="cost">
+                3/5
+            </span>
+        </div>
+    </div>
+    </div>
+    `
 }
 
 
+
+//display restaurant function
 const displayRestaurant = (e)=>{
     if(e.target.id === 'resDiv'){
         const resId = e.target.dataset.name;
@@ -53,9 +84,31 @@ const displayRestaurant = (e)=>{
 
         resCnt.style.display = 'block';
 
-        //Search for the res with id that we get
+        //Search for the result with resId (or name, doesn't matter) and pass into createTemplateFirst
 
-        firstSectionCnt.innerHTML = createTemplate(obj);
+        firstSectionCnt.innerHTML = createTemplateFirst(obj);
+
+        const revButton = document.querySelector('#displayReviews');
+
+
+        //display reviews
+        //Here fetch the reviews and pass object to createTemplateSecond to display (fetch by id/name that we get first)
+
+        //Or fetch reviews at start and save in variable, doesn't matter
+        revButton.addEventListener('click', ()=>{
+            const revCnt = document.querySelector('#restaurantSpecificInfo');
+            revCnt.style.display = 'grid';
+            if(revCnt.classList.contains('revDisplayed')){
+                revCnt.style.display = 'none';
+                revCnt.classList.remove('revDisplayed');
+            }else{
+                revCnt.innerHTML = createTemplateSecond(); 
+                revCnt.classList.add('revDisplayed');
+            }
+            
+
+        });
+
 
     }
 }
