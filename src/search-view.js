9116f27ff770550c75fc952d24c1d67038ec2b89
MichalS <michal.smalira@gmail.com>
2021-01-14 14:40:47 +0100

    styling lists for filters

--- src/search-view.js
+++ src/search-view.js
@@ -1,11 +1,11 @@
 //Importing main function
-const {mainFunc, fetchUserReviews} = require('./restaurants-api');
-const {generateBtn, append, resetState} = require('./feature-pagination');
+const { mainFunc, fetchUserReviews } = require('./restaurants-api');
+const { generateBtn, append, resetState } = require('./feature-pagination');
 const notValid = require('./validate');
 const displayRestaurant = require('./single-res-view');
 
 //Creating template string for restaurant nav
-const pushTemplate = (obj, arr)=>{
+const pushTemplate = (obj, arr) => {
     arr.push(`<div id='resDiv' class='resDiv' data-name="${obj.id}">
                 <span class='resTitle'>
                     ${obj.name}
@@ -23,7 +23,7 @@ const pushTemplate = (obj, arr)=>{
 }
 
 //Display data when click on search button
-function display(e){
+function display(e) {
     resetState()
     let rez;
     //Prevent from reloading page on submit 
@@ -41,157 +41,165 @@ function display(e){
     const inputValue = val.value.trim();
 
     const loading = document.querySelector("#loading");
-    loading.style.display='flex';
-        //Add value of checkbox here where is empty array
-    mainFunc(inputValue).then(function(result){
-            mainSection.style.height = '85%';
-            const buttons = document.querySelector('#paginationContainer');
-            const divData = document.querySelector('#restaurantsNavCon');
-            const filter = document.querySelector('#filterRestaurants');
-            let navData = [];
-            //Validation
-            if(result[0]==='incorrect syntax'){
-                notValid(val);
-                mainSection.style.height = '79%';
-            }else if(result[0]==='city does not exist'){
-                container.style.display = 'grid';
-                divData.style.display = 'none';
-                buttons.style.display = 'none';
-                filter.style.display = 'none';
-                container.innerHTML += 
-                    `
+    loading.style.display = 'flex';
+    //Add value of checkbox here where is empty array
+    mainFunc(inputValue).then(function(result) {
+        mainSection.style.height = '85%';
+        const buttons = document.querySelector('#paginationContainer');
+        const divData = document.querySelector('#restaurantsNavCon');
+        const filter = document.querySelector('.filtred');
+        let navData = [];
+        //Validation
+        if (result[0] === 'incorrect syntax') {
+            notValid(val);
+            mainSection.style.height = '79%';
+        } else if (result[0] === 'city does not exist') {
+            container.style.display = 'grid';
+            divData.style.display = 'none';
+            buttons.style.display = 'none';
+            filter.style.display = 'none';
+            container.innerHTML +=
+                `
                         <div class='townNotFound'>
                             Sorry, we can't find the restaurants in <br> <br> ${inputValue}
                         </div>
                     `;
-                //Scroll to the nav after submit
-                container.scrollIntoView();
-            }else{
-                if(container.contains(document.querySelector('.townNotFound'))){
-                    container.removeChild(document.querySelector('.townNotFound'));
-                }
-                divData.innerHTML ='';
-                buttons.innerHTML ='';
-                filter.innerHTML = '';
-                divData.style.display = 'grid';
-                buttons.style.display = 'flex';
-                filter.style.display = 'grid';
-                //Creating templates with data and pushing into array
-                container.style.display = 'grid';
-                result.forEach((element)=>{
-                    //data-name - get this on click and base on that display restaurant
-                    pushTemplate(element,navData);
-                });
+            //Scroll to the nav after submit
+            container.scrollIntoView();
+        } else {
+            if (container.contains(document.querySelector('.townNotFound'))) {
+                container.removeChild(document.querySelector('.townNotFound'));
+            }
+            divData.innerHTML = '';
+            buttons.innerHTML = '';
+            filter.innerHTML = '';
+            divData.style.display = 'grid';
+            buttons.style.display = 'flex';
+            filter.style.display = 'grid';
+            //Creating templates with data and pushing into array
+            container.style.display = 'grid';
+            result.forEach((element) => {
+                //data-name - get this on click and base on that display restaurant
+                pushTemplate(element, navData);
+            });
 
-                //Default append data on the first site
-                append(navData, buttons, divData);
+            //Default append data on the first site
+            append(navData, buttons, divData);
 
-                //Get the array of all cuisines in the city
-                const cuisinesAll = [];
-                result.forEach((element)=>{
-                    const cuisinesSplit = element.cuisines.split(',');
-                    cuisinesSplit.forEach((cuisine)=>{
-                        if(!cuisinesAll.includes(cuisine.trim()) && cuisine.length >=3){
-                            cuisinesAll.push(cuisine.trim());
+            //Get the array of all cuisines in the city
+            const cuisinesAll = [];
+            result.forEach((element) => {
+                const cuisinesSplit = element.cuisines.split(',');
+                cuisinesSplit.forEach((cuisine) => {
+                    if (!cuisinesAll.includes(cuisine.trim()) && cuisine.length >= 3) {
+                        cuisinesAll.push(cuisine.trim());
+                    }
+                })
+            })
+
+
+            //Display filters
+            cuisinesAll.forEach((element) => {
+                // filter.innerHTML += `
+                //     <label for="${element}" id="filterLabel" class='container'>${element}
+                //     <input type="checkbox" id="${element}" name="cuisineFilter" value="${element}" class="chkId">
+                //     <span class='checkmark'></span>
+                //     </label>
+                // `;
+                filter.innerHTML += `
+                        <li>
+                            <label for="${element}" id="filterLabel" class='container'>${element}
+                            <input type="checkbox" id="${element}" name="cuisineFilter" value="${element}" class="chkId">
+                            <span class='checkmark'></span>
+                            </label>
+                        </li>
+                    `;
+            })
+
+
+            //Saving the array of data
+            let savedNavData = navData;
+
+            const filterRestaurants = (e) => {
+                //Checking target of the event
+                if (e.target.className === 'chkId') {
+                    resetState();
+                    const filterArray = [];
+                    //Getting all the checkboxes
+                    const checkedValues = document.querySelectorAll('.chkId');
+
+                    //Pushing all checked value into the array
+                    [...checkedValues].forEach((element) => {
+                        if (element.checked) {
+                            filterArray.push(element.value);
                         }
                     })
-                })
 
-                
-                //Display filters
-                cuisinesAll.forEach((element)=>{
-                    filter.innerHTML += `
-                        <label for="${element}" id="filterLabel" class='container'>${element}
-                        <input type="checkbox" id="${element}" name="cuisineFilter" value="${element}" class="chkId">
-                        <span class='checkmark'></span>
-                        </label>
-                    `;
-                })
-
-                
-                 //Saving the array of data
-                 let savedNavData = navData;
-
-                const filterRestaurants = (e)=>{
-                    //Checking target of the event
-                    if(e.target.className === 'chkId'){
-                        resetState();
-                        const filterArray = [];
-                        //Getting all the checkboxes
-                        const checkedValues = document.querySelectorAll('.chkId');
-
-                        //Pushing all checked value into the array
-                        [...checkedValues].forEach((element)=>{
-                            if(element.checked){
-                                filterArray.push(element.value);
-                            }
-                        })
-
-                        let tmpNavData = [];
-                        //Getting restaurants with matching cuisines
-                            result.forEach((element)=>{
-                                const splitArr = element.cuisines.split(',');
-                                const formatedArr = splitArr.map(el=>el.trim());
-                                console.log(formatedArr);
-                                const rez = filterArray.some(r => formatedArr.includes(r));
-                                if(rez){
-                                    pushTemplate(element,tmpNavData);
-                                }
-
-                            })
-                        
-                        //Resets data to default when unchecked
-                       
-                        if(tmpNavData.length === 0){
-                            let check = true;
-                            [...checkedValues].forEach((element)=>{
-                                if(element.checked){
-                                    check = false;
-                                }
-                            });
-                            if(check===true){
-                                navData = savedNavData;
-                            }else{
-                                navData = [];
-                            }
-                        }else{
-                            navData = tmpNavData;
+                    let tmpNavData = [];
+                    //Getting restaurants with matching cuisines
+                    result.forEach((element) => {
+                        const splitArr = element.cuisines.split(',');
+                        const formatedArr = splitArr.map(el => el.trim());
+                        console.log(formatedArr);
+                        const rez = filterArray.some(r => formatedArr.includes(r));
+                        if (rez) {
+                            pushTemplate(element, tmpNavData);
                         }
-                        //Displaying filtered data and generate new buttons
-                        generateBtn(e);
-                        append(navData, buttons, divData);
+
+                    })
+
+                    //Resets data to default when unchecked
+
+                    if (tmpNavData.length === 0) {
+                        let check = true;
+                        [...checkedValues].forEach((element) => {
+                            if (element.checked) {
+                                check = false;
+                            }
+                        });
+                        if (check === true) {
+                            navData = savedNavData;
+                        } else {
+                            navData = [];
+                        }
+                    } else {
+                        navData = tmpNavData;
                     }
-                }
-
-                //Run filter
-                filter.addEventListener('click', filterRestaurants);
-
-                //To append pass array with data, element that will contain the buttons, element that will contain data
-                //Add event to generate buttons
-                document.addEventListener('click', (e)=>{
+                    //Displaying filtered data and generate new buttons
                     generateBtn(e);
                     append(navData, buttons, divData);
-                });
-                
-                //Event for display single restaurant
-                document.addEventListener('click', function(e){
-                    const resId = e.target.dataset.name;
-                    if(e.target.id === 'resDiv'){
-                        //Fetching reviews and passing into display function
-                        fetchUserReviews(resId, result).then(function(res){
-                            const resCnt = document.querySelector('article');
-                            resCnt.style.display = 'none';
-                            displayRestaurant(res, resId);
-                            resCnt.scrollIntoView();
-                        });
-                    }
-                });
-
-                //Scroll to the nav after submit
-                container.scrollIntoView();
+                }
             }
-            loading.style.display='none';
-        })
+
+            //Run filter
+            filter.addEventListener('click', filterRestaurants);
+
+            //To append pass array with data, element that will contain the buttons, element that will contain data
+            //Add event to generate buttons
+            document.addEventListener('click', (e) => {
+                generateBtn(e);
+                append(navData, buttons, divData);
+            });
+
+            //Event for display single restaurant
+            document.addEventListener('click', function(e) {
+                const resId = e.target.dataset.name;
+                if (e.target.id === 'resDiv') {
+                    //Fetching reviews and passing into display function
+                    fetchUserReviews(resId, result).then(function(res) {
+                        const resCnt = document.querySelector('article');
+                        resCnt.style.display = 'none';
+                        displayRestaurant(res, resId);
+                        resCnt.scrollIntoView();
+                    });
+                }
+            });
+
+            //Scroll to the nav after submit
+            container.scrollIntoView();
+        }
+        loading.style.display = 'none';
+    })
 }
 //Exporting main function
 module.exports = display;
