6e45adca8db8211e3b77bd915ec7e80f085d6acc
dawid-to <dawidtolloczko@gmail.com>
2021-01-16 00:35:55 +0100

    Transferred emailSentButton from 'Single Restauran View' to 'Favourite List'

--- src/nav-bar.js
+++ src/nav-bar.js
@@ -20,6 +20,39 @@ const displayFav = (e)=>{
         favList.style.width = '0';
         favList.style.right = '-4em';
     }
+
+    //Display and hide modal box via envelope button
+    const openModalButton = document.querySelector('#modalOpenButton');
+    const closeModalButton = document.querySelector('#modalCloseButton');
+    const overlay = document.getElementById('overlay');
+    
+    openModalButton.addEventListener('click', () => {
+        const modal = document.querySelector('.modal');
+        openModal(modal);
+    })
+    
+    function openModal(modal) {
+        if (modal == null) return;
+        overlay.classList.add('active');
+        modal.classList.add('active');
+    }
+
+    closeModalButton.addEventListener('click', () => {
+        const modal = document.querySelector('.modal');
+        closeModal(modal);
+    })
+
+    function closeModal(modal) {
+        if (modal == null) return;
+        modal.classList.remove('active');
+        overlay.classList.remove('active');            
+    }
+
+    //Close modal clicking outside the modal
+    overlay.addEventListener('click', () => {
+        const modal = document.querySelector('.modal.active');
+        closeModal(modal);
+    })
 }
 
 module.exports = displayFav;
