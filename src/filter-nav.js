0763f495b9675f7f2a98eba6a14ed98127258c12
Cyprian Zamiara <veryyapeee@gmail.com>
2021-01-17 20:20:30 +0100

    filter fix nr 2009321

--- src/filter-nav.js
+++ src/filter-nav.js
@@ -26,16 +26,17 @@ const filterNav = (dom)=>{
         let clicked = 0;
         filterButton.addEventListener('click',()=>{
             if(clicked === 0){
-                restFiltersCnt.style.maxHeight = '5em';
-                restFiltersCnt.style.transition = 'all 0.3s ease-in';
-                restFiltersCnt.style.visibility = 'visible';
+                restFiltersCnt.style.transform = 'translateY(0)';
+                restFiltersCnt.style.transition = 'all 0.3s ease-out;';
+                restFiltersCnt.style.zIndex = '999';
+                filterButton.style.transform = 'translateY(0)';
                 filterButton.innerHTML = 'Hide';
                 clicked++;
             }else if(clicked === 1){
-                restFiltersCnt.style.maxHeight = '0';
-                restFiltersCnt.style.transition = 'all 0.3s ease-in';
-                restFiltersCnt.style.visibility = 'hidden';
-                filterButton.classList.add('notFiltered');
+                restFiltersCnt.style.transform = 'translateY(-100%)';
+                filterButton.style.transform = 'translateY(-100%)';
+                restFiltersCnt.style.transition = 'all 0.3s ease-out;';
+                restFiltersCnt.style.zIndex = '-9999';
                 filterButton.innerHTML = 'Show more filters';
                 clicked--;
             }
