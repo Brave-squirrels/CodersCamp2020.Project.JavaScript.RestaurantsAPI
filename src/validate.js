70b4a98eca9a1fdd6ac091ab3354b5cae373726a
kam237zasada <kam237zasada@wp.pl>
2021-01-17 10:58:55 +0100

    deploy

--- src/validate.js
+++
@@ -1,8 +0,0 @@
-const notValid = (inputDOM) => {
-    inputDOM.classList.add("invalid");
-    setTimeout(function(){
-        inputDOM.classList.remove('invalid');
-    }, 500);
-}
-
-module.exports = notValid;
